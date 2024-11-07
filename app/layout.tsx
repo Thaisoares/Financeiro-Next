fd62cc77cc1573bba71892bddfe80cc6832685fd
Thaisoares <thais.soares.lamas@gmail.com>
2024-11-07 20:04:45 -0300

    chore: chage font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,10 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Mulish } from "next/font/google";
 import "./globals.css";
 import SessionProviderWrapper from "./_components/sessionProviderWrapper";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const mulish = Mulish({
+  subsets: ["latin-ext"],
 });
 
 export const metadata: Metadata = {
@@ -26,9 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${mulish.className} antialiased`}>
         <SessionProviderWrapper>{children}</SessionProviderWrapper>
       </body>
     </html>
